f7074531726991bf4def2b0b78e517bcd0311298
xin1896 <chenjingtt5@gmail.com>
2023-04-10 11:05:02 +0800

    Your commit message

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -580,7 +580,7 @@ export function Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>Jing's ChatGPT</div>
           <div className={styles["sidebar-sub-title"]}>
             Build your own AI assistant.
           </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "Jing's ChatGPT",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Jing's ChatGPT",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa"
